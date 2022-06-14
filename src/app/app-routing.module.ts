aad74ad26b41b865d1133dd063324730de331ec0
Джигурда Антон Алексеевич <adzhigurda@lnx-vdi-t092.aorti.ru>
2022-06-14 14:02:47 +0300

    feature. CRUD User, Change theme, Select context

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Example1Component } from './example/example1.component';
+import { Example2Component } from './example2/example2.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'upload'},
+  {path: 'upload', pathMatch: 'full', component: Example1Component},
+  {path: 'state',  pathMatch: 'full', component: Example2Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { Example1Component } from './example/example1.component';  
+import { Example2Module } from './example2/example2.module';
+import { FileUploadModule } from './file-upload/file-upload.module';
+import { TuiRootModule } from '@taiga-ui/core';
+import { ItemMenuComponent } from './shared/item-menu/item-menu.component';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools'; 
+import { environment } from '../environments/environment';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Example1Component,
+    ItemMenuComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,    
+    AppRoutingModule,
+    FileUploadModule,	
+    TuiRootModule,
+    Example2Module, 
+    StoreModule.forRoot({}), 
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
   ],
   providers: [],
   bootstrap: [AppComponent]
