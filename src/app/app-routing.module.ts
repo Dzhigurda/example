00282976ba1a6c14a9ff12f95d4e22704d4b5213
Джигурда Антон Алексеевич <adzhigurda@lnx-vdi-t004.aorti.ru>
2022-06-15 05:36:38 +0300

    UI, remove bottom menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { Example1Component } from './example/example1.component';
 import { Example2Component } from './example2/example2.component';
 
 const routes: Routes = [
-  {path: '', pathMatch: 'full', redirectTo: 'upload'},
+  {path: '', pathMatch: 'full',  component: Example2Component},
   {path: 'upload', pathMatch: 'full', component: Example1Component},
   {path: 'state',  pathMatch: 'full', component: Example2Component}
 ];
